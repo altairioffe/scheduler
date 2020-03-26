f86eb90d6e9b9737f28928d476e14f0672cd0c42
Altair <altair.ioffe@gmail.com>
2020-03-26 14:17:55 -0400

    refactored with application data file

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,29 +3,19 @@ import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import axios from "axios";
+import useApplicationData from "../hooks/useApplicationData";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors"
 
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
 
-  const setDay = day => setState({ ...state, day });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteHandler
+  } = useApplicationData();
 
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get("http://localhost:8001/api/days")),
-      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
-      Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
-    ]).then(all => {
-      setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
-  }, []);
 
   const appointments = getAppointmentsForDay(state, state.day)
   const interviewers = getInterviewersForDay(state, state.day)
@@ -33,50 +23,6 @@ export default function Application(props) {
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
 
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    console.log('FROM BOOOK INTERVIEWW, id: ', id, ' interview: ', interview);
-    return axios.put(`http://localhost:8001/api/appointments/${id}`,  { interview: interview })
-    .then(() => setState({...state, appointments}))
-  }
-
-  // function save(name, interviewer) {
-  //   const interview = {
-  //     student: name,
-  //     interviewer: interviewer.id
-  //   };
-    
-  //   bookInterview(appointment.id, interview)
-  // }
-
-  function deleteHandler(id) {
-    console.log('clicked')
-    const interview = null
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-   return axios.delete(`http://localhost:8001/api/appointments/${id}`,  { interview: interview })
-    .then((res) => console.log("success: ", res))
-    .then(setState({...state, appointments}))
-   // .catch(err => console.log("ERROR CATCHED: ", err))
-
-  }
-
-  //console.log('application js ', appointment.id, interview)
     return (
       <Appointment
         key={appointment.id}
@@ -85,7 +31,6 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
-       // onSave={save}
         deleteHandler={deleteHandler}
         bookInterview={bookInterview}
       />
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+export default function useApplicationData(props) {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get("http://localhost:8001/api/days")),
+      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
+      Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
+    ]).then(all => {
+      setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    });
+  }, []);
+
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    //console.log('FROM BOOOK INTERVIEWW, id: ', id, ' interview: ', interview);
+    return axios.put(`http://localhost:8001/api/appointments/${id}`,  { interview: interview })
+    .then(() => setState({...state, appointments}))
+  }
+
+  function deleteHandler(id) {
+
+    const interview = null
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+   return axios.delete(`http://localhost:8001/api/appointments/${id}`,  { interview: interview })
+    .then((res) => console.log("success: ", res))
+    .then(setState({...state, appointments}))
+   // .catch(err => console.log("ERROR CATCHED: ", err))
+
+  }
+
+  return(
+   { state, setDay, bookInterview, deleteHandler }
+  )
+}
